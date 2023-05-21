3ac9fa115a65f3c43e0eac15850e7395c6bb16a8
Usachev13 <2893318@gmail.com>
2023-05-21 09:16:14 +0300

    task1 done

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
 public class Main {
-    //hw6
+    public static void main(String[] args) {
+        /* task1 */
+        for ( int i = 1; i <= 10; i += 1){
+            System.out.println("итерация цикла " + i);
+        }
+    }
 }
